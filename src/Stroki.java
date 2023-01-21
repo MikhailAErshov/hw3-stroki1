b9f9e4318b13db3dcc73270e1bfda5ea650940b8
MikhailAErshov <aem.cmd@gmail.com>
2023-01-21 20:10:25 +0300

    stroki is solved

--- src/Stroki.java
+++ src/Stroki.java
@@ -1,2 +1,18 @@
+//Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String.startsWith().
+//Найти позицию подстроки “Java” в строке “I like Java!!!”.
+//Заменить все символы “а” на “о”.
+//Преобразуйте строку к верхнему регистру.
+//Преобразуйте строку к нижнему регистру.
+//Вырезать строку Java c помощью метода String.substring().
+
 public class Stroki {
+    public static void main(String[] args) {
+        String str1 = "I like Java!!!";
+        System.out.println(str1.startsWith("I like"));
+        System.out.println(str1.indexOf("Java"));
+        System.out.println(str1.replace("a", "o"));
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1.replace(str1.substring(str1.indexOf("Java"),str1.indexOf("!!!")), ""));
+    }
 }
